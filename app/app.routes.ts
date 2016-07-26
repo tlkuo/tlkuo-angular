c22c51a0d46db625e3c2895f9972e284e028c386
martin <tlkuo.frb@gmail.com>
2016-07-26 14:00:33 +0800

    Add dashboard route

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,10 +1,15 @@
 import { provideRouter, RouterConfig }  from '@angular/router';
 import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
 
 const routes: RouterConfig = [
   {
     path: 'heroes',
     component: HeroesComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
   }
 ];
 
