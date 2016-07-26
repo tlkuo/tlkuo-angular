fc9a1508fb82eca1cb3f07624dc11f3332e3822b
martin <tlkuo.frb@gmail.com>
2016-07-26 14:48:50 +0800

    Display dashboard top heroes

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -77,7 +77,7 @@ export class HeroesComponent implements OnInit {
   constructor(private heroService: HeroService) { }
 
   getHeroes() {
-    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
 
   ngOnInit() {
