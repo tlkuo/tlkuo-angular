27fd12e7a60be6c3742c9e0383c4547129548191
martin <tlkuo.frb@gmail.com>
2016-07-26 12:25:05 +0800

    Add routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core'
-import { HeroesComponent } from './heroes.component'
+import { ROUTER_DIRECTIVES } from '@angular/router';
 import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <my-heroes></my-heroes>
+    <a [routerLink]="['/heroes']">Heroes</a>
+    <router-outlet></router-outlet>
   `,
-  directives: [HeroesComponent],
+  directives: [ROUTER_DIRECTIVES],
   providers: [HeroService]
 })
 
--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,7 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { AppComponent } from './app.component';
+import { appRouterProviders } from './app.routes';
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+  appRouterProviders
+]);
